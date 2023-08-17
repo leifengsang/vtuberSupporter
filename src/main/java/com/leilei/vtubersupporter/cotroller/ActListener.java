8c5c6a6ee360222141d575da276f1d10f0218eb3
leifengsang <575617231@qq.com>
2023-08-18 04:04:25 +0800

    无效引用

--- src/main/java/com/leilei/vtubersupporter/cotroller/ActListener.java
+++ src/main/java/com/leilei/vtubersupporter/cotroller/ActListener.java
@@ -2,7 +2,6 @@ package com.leilei.vtubersupporter.cotroller;
 
 import com.leilei.vtubersupporter.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
